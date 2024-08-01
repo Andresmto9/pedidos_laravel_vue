c83afee0189e2577e4888ffc54b3c4456473480a
Andresmto9 <andres.dev97@gmail.com>
2024-07-31 22:52:57 -0500

    Cargue archivos proyecto Ticket Shop

---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hola mundo :D
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        async mounted() {
+            console.log("Hola :D")
+        }
+    }
+</script>
